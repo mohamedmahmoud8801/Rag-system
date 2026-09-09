ad056fc0a408bad1484cda2b4a81f200e2d9f165
Mohamed Mahmoud <mohamedmahmoud8801@gmail.com>
2026-09-09 21:41:15 +0300

    modify database from mongo to postgress

--- Rag-system/src/main.py
+++ Rag-system/src/main.py
@@ -5,13 +5,18 @@ from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 from stores.llm.templates.template_parser import TemplateParser
+from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
 async def startup_span():
     settings = get_settings()
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    # app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+    app.db_engine = create_async_engine(postgres_conn)
+    app.db_client = sessionmaker(app.db_engine,class_=AsyncSession,expire_on_commit=False)
+    # app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
     llm_provider_factory = LLMProviderFactory(settings)
     vectordb_provider_factory = VectorDBProviderFactory(settings)
     #generation client
@@ -29,8 +34,9 @@ async def startup_span():
     app.template_parser = TemplateParser(language=settings.PRIMARY_LANG,default_language=settings.DEFAULT_LANG)
 
 
-async def shutdown_span():
-    app.mongo_conn.close()
+async def shutdown_span():  
+    # app.mongo_conn.close()
+    app.db_engine.dispose ()
     app.vectordb_client.disconnect()
 
 
