4df8fcbcda96f7e124a036213b90935ca53468c8
Mohamed Mahmoud <mohamedmahmoud8801@gmail.com>
2026-09-13 13:10:39 +0300

    update providers for pgvector

--- Rag-system/src/stores/vectordb/VectorDBEnums.py
+++ Rag-system/src/stores/vectordb/VectorDBEnums.py
@@ -2,7 +2,26 @@ from enum import Enum
 
 class VectorDBEnums(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR = "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
     COSINE = "cosine"
     DOT = "DOT"
+
+class PgVectorTableSchemeEnums(Enum):
+    ID = "id"
+    TEXT = "text"
+    VECTOR = "vector"
+    CHUNK_ID = "chunk_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector_"
+
+
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_dot_ops"
+
+class PgVectorIndexTypeEnums(Enum):
+    IVFFLAT = "ivfflat"
+    HNSW = "hnsw"
+
--- Rag-system/src/stores/vectordb/VectorDBInterface.py
+++ Rag-system/src/stores/vectordb/VectorDBInterface.py
@@ -21,6 +21,10 @@ class VectorDBInterface(ABC):
     def list_all_collections(self) -> list:
         pass
 
+    @abstractmethod
+    def get_collection_info(self, collection_name: str) -> dict:
+        pass
+
 
     @abstractmethod
     def delete_collection(self, collection_name:str) :
